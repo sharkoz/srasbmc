318d831ecfa98b7b7d763cb687810ff6e2f71c0a
Cosme <sharkoz@gmail.com>
2014-05-26 18:30:26 +0200

    fix when no action

--- webstart/index.php
+++ webstart/index.php
@@ -7,10 +7,11 @@
 <img style="max-width: 100%;" src="./xbmc.png">
 <p><h1>XBMC current status :
 <?
+if(isset($_GET["action"])) {
 if ($_GET["action"] == "stop") exec ('sudo /sbin/initctl stop xbmc');
 
 if ($_GET["action"] == "start") exec ('sudo /sbin/initctl start xbmc');
-
+}
 $status = exec ('sudo /sbin/initctl status xbmc');
 if (strpos($status, "running") == 11)
     echo '<font color="green">Running</font></h1></br></br><a href="index.php?action=stop"><img style="max-width: 50%;" src="1.png" onmouseover="this.src=\'0.png\';" onmouseout="this.src=\'1.png\';" /></a>';
