ef0776574791a21f29ed68a1a094e023b41b527e
Cosme <sharkoz@gmail.com>
2014-05-26 18:26:48 +0200

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Readme file
+
+ **** WEBSTART **** 
+Start/stop xbmc from a webpage.
+
+The page reflects XBMC current status, and allows to start or stop it with a single button.
+
+The apache user (usually www-data) needs to have passwordless sudo on these initctl commands :
+- initctl status xbmc
+- initctl start xbmc
+- initctl stop xbmc
+
+
---
+++ webstart/index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title>XBMC start/stop</title>
+</head>
+<body bgcolor="black" text="white">
+<center>
+<img style="max-width: 100%;" src="./xbmc.png">
+<p><h1>XBMC current status :
+<?
+if ($_GET["action"] == "stop") exec ('sudo /sbin/initctl stop xbmc');
+
+if ($_GET["action"] == "start") exec ('sudo /sbin/initctl start xbmc');
+
+$status = exec ('sudo /sbin/initctl status xbmc');
+if (strpos($status, "running") == 11)
+    echo '<font color="green">Running</font></h1></br></br><a href="index.php?action=stop"><img style="max-width: 50%;" src="1.png" onmouseover="this.src=\'0.png\';" onmouseout="this.src=\'1.png\';" /></a>';
+else
+    echo '<font color="red">Stopped</font></h1></br></br><a href="index.php?action=start"><img style="max-width: 50%;" src="0.png" onmouseover="this.src=\'1.png\';" onmouseout="this.src=\'0.png\';" /></a>';
+?></p>
+
+</body></html>
